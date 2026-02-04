0d308716a26a68820044929171b3ab1783d18cd3
Trainingdlu <trainingcqy@gmail.com>
2026-02-04 17:21:46 +0800

    feat:url

--- sql/analytics/_algo_gravity_ranking.sql
+++ sql/analytics/_algo_gravity_ranking.sql
@@ -1,11 +1,14 @@
 -- [Algorithm] Hacker News Gravity Formula
 -- Logic: 模拟 HN 排名算法，平衡热度与时间衰减，寻找当前"上升速度最快"的新闻
-
 SELECT 
     title_cn AS "标题",
     points AS "热度",
     hours_ago AS "发布时长",
+    url AS "链接", 
+    
     ROUND((points::numeric) / POWER((hours_ago + 2), 1.8), 2) AS "重力得分"
+
 FROM view_dashboard_news
 WHERE hours_ago < 48
-ORDER BY 4 DESC LIMIT 5;
+ORDER BY 5 DESC
+LIMIT 5;
